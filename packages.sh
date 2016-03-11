93390e82caaf623934e0d0593c401d78c4466d9f
fragforce <deploy@fragforce.org>
2016-03-11 10:50:57 -0500

    Cleaned up packages a bit moer

--- packages.sh
+++ packages.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-yum install -y pam-devel git autoconf automake gcc libtool vim samba parted man xfsprogs wget tcpdump unzip screen
+yum install -y pam-devel git vim samba parted man xfsprogs wget tcpdump unzip screen
 echo "<<Installing NTP>>"
 yum install -y ntp ntpdate ntp-doc
 chkconfig ntpd on
 service ntpd start
 echo "<<NTP Configured>>"
+echo "<<Installing Make Packages>>"
+yum install -y autoconf automake gcc libtool
+echo "<<Finished installing Make packages>>"
