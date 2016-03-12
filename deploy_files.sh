71de5b3483c9e0521e039938d6c08ac86ea64436
Fragforce <deploy@fragforce.org>
2016-03-11 23:47:52 -0500

    Added basic samba config to deploy

--- deploy_files.sh
+++ deploy_files.sh
@@ -5,3 +5,8 @@ echo "<<SSH>>"
 sudo cp files/etc/ssh/sshd_config /etc/ssh/
 sudo chown root:root /etc/ssh/sshd_config
 sudo service sshd restart
+echo "<<SAMBA>>"
+sudo cp files/etc/samba/smb.conf /etc/samba
+sudo chown root:root /etc/samba/smb.conf
+sudo chmod 644 /etc/samba/smb.conf
+sudo service smb restart
