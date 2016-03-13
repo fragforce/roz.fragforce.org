ad26ded956d0e11b37e03c68240b0bab8ffb42eb
Fragforce <deploy@fragforce.org>
2016-03-13 14:34:08 -0400

    Updated FTB version to latest

--- minecraft/settings.sh
+++ minecraft/settings.sh
@@ -12,10 +12,10 @@ export JRE_FILE="jre-8u74-linux-x64.tar.gz"
 export JRE_FOLDER="jre1.8.0_74"
 export JRE_BIN=${BASEDIR}/${JRE_FOLDER}/bin/
 export JAVACMD="${JRE_BIN}/java"
-export JAVAOPTS="-server -Xms512M -Xmx2048M -XX:PermSize=256M -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar FTBServer-1.7.10-1558.jar nogui"
+export JAVAOPTS="-server -Xms512M -Xmx2048M -XX:PermSize=256M -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar FTBServer-1.7.10-1614.jar nogui"
 
 #Modpack Variables
-export MODPACK_URL="http://ftb.cursecdn.com/FTB2/modpacks/FTBInfinity/2_3_5/FTBInfinityServer.zip"
+export MODPACK_URL="http://ftb.cursecdn.com/FTB2/modpacks/FTBInfinity/2_4_2/FTBInfinityServer.zip"
 export MODPACK_FILE="FTBInfinityServer.zip"
 export MODPACK_DIR=${SERVERDIR}
 
--- minecraft/setup.sh
+++ minecraft/setup.sh
@@ -5,7 +5,7 @@ if -u ${USER} &>/dev/null; then
   echo "User ${USER} is missing, please run users.sh first"; 
   exit 1;
 fi
-sudo mkdir -p ${BASEDIR}/${SERVERDIR}
+sudo mkdir -p ${SERVERDIR}
 cd ${BASEDIR}
 if [ ! -d ${BASEDIR}/${JRE_FOLDER} ]; then
   echo "<<Downloading JRE>>"
@@ -22,28 +22,35 @@ if [ ! -f ${SERVERDIR}/eula.txt ]; then
 else
   echo "<<Modpack already present>>"
 fi
+function check_config {
+  if [ -z $1 ]; then
+    echo "<<Parameter missing for check_config function>>"
+  else
+    if [ ! -f ${FILES}/$1 ]; then
+      echo "<<File $1 missing from ${FILES} directory>>"
+    else
+      if ! sudo cmp -s ${FILES}/$1 ${SERVERDIR}/$1; then
+        echo "<<Updating $1>>"
+        if [ -z $2 ]; then
+          sudo mkdir -p ${FILES}/$2
+          echo "created ${FILES} $2"
+        fi
+        sudo cp ${FILES}/$1 ${SERVERDIR}/$1
+      fi
+    fi
+  fi
+}
 if ! sudo cmp -s ${CWD}/settings.sh ${SERVERDIR}/settings.sh; then
   echo "<<Updating settings.sh>>"
   sudo cp ${CWD}/settings.sh ${SERVERDIR}/settings.sh
 fi
-if ! sudo cmp -s ${FILES}/eula.txt ${SERVERDIR}/eula.txt; then
-  echo "<<Updating eula.txt>>"
-  sudo cp ${FILES}/eula.txt ${SERVERDIR}/eula.txt
-fi
-if ! sudo cmp -s ${FILES}/server.properties ${SERVERDIR}/server.properties; then
-  echo "<<Updating server.properties>>"
-  sudo cp ${FILES}/server.properties ${SERVERDIR}/server.properties
-fi
-if ! sudo cmp -s ${FILES}/ops.json ${SERVERDIR}/ops.json; then
-  echo "<<Updating ops.json>>"
-  sudo cp ${FILES}/ops.json ${SERVERDIR}/ops.json
-fi
-if ! sudo cmp -s ${FILES}/local/ftbu/config.json ${SERVERDIR}/local/ftbu/config.json
+check_config eula.txt
+check_config server.properties
+check_config ops.json
+check_config run.sh
+if ! sudo cmp -s ${FILES}/local/ftbu/config.json ${SERVERDIR}/local/ftbu/config.json; then
   echo "<<Updating config.json>>"
-fi
-if ! sudo cmp -s ${FILES}/run.sh ${SERVERDIR}/run.sh; then
-  echo "<<Updating run.sh>>"
-  sudo cp ${FILES}/run.sh ${SERVERDIR}/run.sh;
-  sudo chmod a+x ${SERVERDIR}/run.sh
+  sudo mkdir -p ${SERVERDIR}/local/ftbu
+  sudo cp ${FILES}/local/ftbu/config.json ${SERVERDIR}/local/ftbu/config.json
 fi
 sudo chown -R ${USER}:${GROUP} ${BASEDIR}
