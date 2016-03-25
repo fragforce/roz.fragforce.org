a4711af8f0e6964acb66a22108e41ef28733e76b
Fragforce <deploy@fragforce.org>
2016-03-25 17:40:27 -0400

    Added a script to add exfat support, and to remove the repository afterwards

---
+++ exfat.sh
@@ -0,0 +1,4 @@
+sudo yum -y install epel-release
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+sudo yum -y install exfat-utils fuse-exfat
+sudo yum -y remove nux-dextop-release
